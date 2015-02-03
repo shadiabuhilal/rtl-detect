41e8cfdc168cbace03783a9bfb94dae5a7fc0905
shadi abu hilal <shadiabuhilal@gmail.com>
2015-02-02 17:05:13 -0800

    update license

--- test/lib/rtl-detect-private_test.js
+++ test/lib/rtl-detect-private_test.js
@@ -1,4 +1,7 @@
-//Copyright 2015, Yahoo! Inc.
+/**
+ * Copyright 2015, Yahoo! Inc.
+ * Copyrights licensed under the New BSD License. See the accompanying LICENSE file for terms.
+ */
 'use strict';
 
 var RtlDetect = require('../../' + 'lib/rtl-detect');
